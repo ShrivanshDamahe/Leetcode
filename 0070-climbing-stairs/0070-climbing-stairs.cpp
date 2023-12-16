b3f4684d0e37bc6be4e57be8d2ea1cdb3e908cc0
ShrivanshDamahe <140092918+ShrivanshDamahe@users.noreply.github.com>
2023-12-16 14:55:00 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (87.59%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if (n == 1)
+            return 1;
+        else if (n == 2)
+            return 2;
+        
+        int a = 1, b = 2, c = 0;
+        for (int i = 0; i < (n - 2); i++){
+            c = a + b;
+            a = b;
+            b = c;
+        }
+
+        return c;
+    }
+    // Time complexity - O(n)
+};
