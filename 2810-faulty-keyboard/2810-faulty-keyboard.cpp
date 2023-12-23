a49b77408cd4b6d4ad6fb0fbbf527c00b2e99128
ShrivanshDamahe <140092918+ShrivanshDamahe@users.noreply.github.com>
2023-12-23 20:44:13 +0530

    Time: 8 ms (37.53%), Space: 6.9 MB (62.91%) - LeetHub

---
+++ 2810-faulty-keyboard/2810-faulty-keyboard.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string finalString(string s) {
+        string ans = "";
+
+        for (int i = 0; i < s.size(); i++){
+            if (s[i] != 'i')
+                ans += s[i];
+            else
+                reverse(ans.begin(), ans.end());
+        }
+
+        return ans;
+    }
+    // Time complexity - O(n)
+};
