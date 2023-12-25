c8b89603d90fafd8dade504df8d6e643a22e5a2f
ShrivanshDamahe <140092918+ShrivanshDamahe@users.noreply.github.com>
2023-12-25 17:36:12 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (65.44%) - LeetHub

---
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int numDecodings(string s) {
+        if (s.empty() || s[0] == '0') 
+            return 0;
+
+        vector<int> ans(s.length() + 1, 0);
+        ans[0] = 1;
+        ans[1] = 1;
+
+        for (int i = 2; i <= s.length(); i++){
+            int temp1 = s[i - 1] - '0';
+            int temp2 = (s[i - 2] - '0') * 10 + (s[i - 1] - '0');
+
+            if (temp1 >= 1) {
+                ans[i] += ans[i - 1];
+            }
+
+            if (temp2 >= 10 && temp2 <= 26) {
+                ans[i] += ans[i - 2];
+            }
+        }
+
+        return ans[s.length()];
+    }
+    // Time complexity - O(n)
+};
