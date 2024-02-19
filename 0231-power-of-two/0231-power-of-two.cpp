8a0a81680b4cefb6896d549af7d6c14ec177c89d
ShrivanshDamahe <140092918+ShrivanshDamahe@users.noreply.github.com>
2024-02-19 23:10:47 +0530

    Time: 3 ms (26.38%), Space: 7.2 MB (19.05%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,18 +1,23 @@
 class Solution {
 public:
+    // bool isPowerOfTwo(int n) {
+    //     if (n <= 0)
+    //         return 0;
+
+    //     while (n % 2 == 0)
+    //         n /= 2;
+
+    //     return (n == 1);
+    // }
+    // Time complexity - O(log n)
+    // Space complexity - O(1)
+
     bool isPowerOfTwo(int n) {
-        bool flag = 0;
         if (n <= 0)
             return 0;
 
-        while (n % 2 == 0){
-            n /= 2;
-        }
-        
-        if (n == 1)
-            flag = 1;
-
-        return flag;
+        return ((n & (n - 1)) == 0);
     }
-    // Time complexity - O(log n)
+    // Time complexity - O(1)
+    // Space complexity - O(1)
 };
