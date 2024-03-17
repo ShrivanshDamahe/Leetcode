f372aa6289c00a6e047bb185ba570f1479847f02
ShrivanshDamahe <140092918+ShrivanshDamahe@users.noreply.github.com>
2024-03-17 16:32:22 +0530

    Time: 0 ms (100.00%), Space: 17.6 MB (8.07%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.cpp
+++ 0101-symmetric-tree/0101-symmetric-tree.cpp
@@ -10,22 +10,50 @@
  * };
  */
 class Solution {
-private:
-    bool check(TreeNode* l, TreeNode* r) {
-        if (!l && !r)
-            return true;
-        else if ((l && !r) || (!l && r))
-            return false;
-        else if (l->val == r->val)
-            return check(l->left, r->right) && check(l->right, r->left);
-        return false;
-    }
+// private:
+//     bool check(TreeNode* l, TreeNode* r) {
+//         if (!l && !r)
+//             return true;
+//         else if (!l || !r)
+//             return false;
+//         else if (l->val == r->val)
+//             return check(l->left, r->right) && check(l->right, r->left);
+//         return false;
+//     }
 
 public:
-    bool isSymmetric(TreeNode* root) {
-        return check(root->left, root->right);
-    }
+    // bool isSymmetric(TreeNode* root) {
+    //     return check(root->left, root->right);
+    // }
     // Time complexity - O(n)
     // Space complexity - O(h)
     // where, h - height of tree
+
+    bool isSymmetric(TreeNode* root) {
+        queue<pair<TreeNode*, TreeNode*>> box;
+        box.push({root->left, root->right});
+
+        while (!box.empty()){
+            int temp = box.size();
+            while (temp--){
+                pair<TreeNode*, TreeNode*> set = box.front();
+                box.pop();
+
+                if (!set.first && !set.second)
+                    continue;
+                else if (!set.first || !set.second)
+                    return false;
+                else if (set.first->val == set.second->val){
+                    box.push({set.first->left, set.second->right});
+                    box.push({set.first->right, set.second->left});
+                }
+                else 
+                    return false;
+            }
+        }
+
+        return true;
+    }
+    // Time complexity - O(n)
+    // Space complexity - O(n)
 };
